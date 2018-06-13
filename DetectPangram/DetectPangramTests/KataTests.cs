d7c806fa282d089d48ba442c8a737b1cd622b8a2
Ouch1978 <ouch1978@hotmail.com>
2018-06-13 23:03:44 +0800

    Initial commit

---
+++ DetectPangram/DetectPangramTests/KataTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void IsPangramTest()
+        {
+            Assert.AreEqual( true , Kata.IsPangram( "The quick brown fox jumps over the lazy dog." ) );
+        }
+    }
+}
